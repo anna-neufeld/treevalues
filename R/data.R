f9028e462e0e6f3cff283fcf93e38dc76ea1a87c
Anna Neufeld <annaneufeld@Annas-MacBook-Pro-2.local>
2021-06-02 11:35:09 -0700

    data is working and I am down to just 1 warning???? wowgit add .

--- R/data.R
+++ R/data.R
@@ -2,32 +2,32 @@
 #'
 #' @format
 #' \describe{
-#'   \item trt. Treatment
-#'   \item sex. Sex
-#'   \item bmi0. BMI
-#'   \item snackkcal0. Snacking kilo calories
-#'   \item srvgfv0. Serving size of fruits and vegetables
-#'   \item srvgssb0. Serving size of beverages
-#'   \item kcal24h0.
-#'   \item edeq01.
-#'   \item edeq02.
-#'   \item edeq13.
-#'   \item edeq14.
-#'   \item edeq15.
-#'   \item edeq22.
-#'   \item edeq23.
-#'   \item edeq25.
-#'   \item edeq26.
-#'   \item cdrsbody0. Body image
-#'   \item weighfreq0. Weighing frequency
-#'   \item freqff0. Fast food frequency
-#'   \item age. Age
-#'   \item tfactor1.
-#'   \item tfactor2.
-#'   \item tfactor3.
-#'   \item mlhfbias0.
-#'   \item fwahfbias0.
-#'   \item rrvfood. Relative reinforcement of food
+#'   \item{trt}{Treatment}
+#'   \item{sex}{Sex}
+#'   \item{bmi0}{BMI}
+#'   \item{skcal}{Snacking kilo calories.}
+#'   \item{srvgfv0}{Serving size of fruits and vegetables.}
+#'   \item{srvgssb0}{Serving size of beverages}
+#'   \item{kcal24h0}{Response variable; kcals consumed in 24 hours.}
+#'   \item{disinhibition}{}
+#'   \item{wanting}{}
+#'      \item{hunger}{}
+#'         \item{liking}{}
+#'            \item{resteating}{}
+#'   \item{edeq01}{}
+#'   \item{edeq02}{}
+#'   \item{edeq13}{}
+#'   \item{edeq14}{}
+#'   \item{edeq15}{}
+#'   \item{edeq22}{}
+#'   \item{edeq23}{}
+#'   \item{edeq25}{}
+#'   \item{edeq26}{}
+#'   \item{cdrsbody0}{Body image}
+#'   \item{weighfreq0}{Weighing frequency}
+#'   \item{freqff}{Fast food frequency}
+#'   \item{age}{Age}
+#'   \item{rrvfood}{Relative reinforcement of food}
 #'}
 #' @keywords datasets
 #' @name blsdata
