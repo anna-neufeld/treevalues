c197d5f6641a73f4c7b89024a83bd27246b9e85f
Anna Neufeld <annaneufeld@Annas-MacBook-Pro-2.local>
2021-06-02 10:40:16 -0700

    data file

---
+++ R/blsdata.R
@@ -0,0 +1,18 @@
+#' Dataset from the Box Lunch Study, taken from the "visTree" package.
+#'
+#' Information
+#'
+#' @docType data
+#'
+#' @usage data(blsdata)
+#'
+#' @keywords datasets
+#'
+#' @references
+#' (\href{https://github.com/AshwiniKV/visTree}{https://github.com/AshwiniKV/visTree})
+#'
+#' @source \href{https://github.com/AshwiniKV/visTree/tree/master/data}{https://github.com/AshwiniKV/visTree/tree/master/data}
+#'
+#' @examples
+#' data(blsdata)
+"blsdata"
